ae1b5b60cf3bf44752a280e1d44c1c7780868069
Locitao <rvdtera@gmail.com>
2017-06-05 22:35:59 +0200

    Fixed the post for stolen cars

--- src/main/java/com/dreamteam/police/remote/RemoteReporting.java
+++ src/main/java/com/dreamteam/police/remote/RemoteReporting.java
@@ -1,6 +1,7 @@
 package com.dreamteam.police.remote;
 
 import com.dreamteam.police.dto.StolenDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.vaadin.spring.annotation.SpringComponent;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by Loci on 15-5-2017.
@@ -25,15 +29,17 @@ public class RemoteReporting {
         HttpHeaders headers = authentication.getHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        //MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         Gson gson = new Gson();
         String json = gson.toJson(stolenDTO);
-        map.add("stolenDTO", json);
+        //Map<String, String> map = new ObjectMapper().readValue(json, Map.class);
+        //map.add("stolen", json);
 
-        HttpEntity<?> request = new HttpEntity<>(map, headers);
+        HttpEntity<?> request = new HttpEntity<>(json, headers);
 
         try {
-            ResponseEntity<String> response = restTemplate.exchange("http://192.168.24.33:8080/dreamteam-administration/police/api/cars/" + stolenDTO.getCarDTO().getIcan(), HttpMethod.POST, request, String.class);
+            //ResponseEntity<String> response = restTemplate.exchange("http://192.168.24.33:8080/dreamteam-administration/police/api/cars/" + stolenDTO.getCarDTO().getIcan(), HttpMethod.POST, request);
+            ResponseEntity<String> response = restTemplate.postForEntity("http://192.168.24.33:8080/dreamteam-administration/police/api/cars/" + stolenDTO.getCarDTO().getIcan(), request, String.class);
         } catch (HttpClientErrorException ex) {
             System.out.println("api not online yet.");
             ex.printStackTrace();
